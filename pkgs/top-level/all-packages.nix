c4bc1a7edd5b2673d367b39786e4f062001c5357
Olli Helenius <liff@iki.fi>
2025-08-28 23:11:16 +0300

    pianoteq: reinit at 9.2.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8988,8 +8988,6 @@ with pkgs;
     ocamlPackages = ocaml-ng.ocamlPackages_4_14;
   };
 
-  pianoteq = callPackage ../applications/audio/pianoteq { };
-
   pidginPackages = recurseIntoAttrs (
     callPackage ../applications/networking/instant-messengers/pidgin/pidgin-plugins { }
   );
