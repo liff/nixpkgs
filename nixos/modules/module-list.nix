c6352d59b59f383aa8208d73e45d24858c1f6313
Olli Helenius <liff@iki.fi>
2026-06-22 22:02:31 +0300

    nixos/jaeger: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1070,6 +1070,7 @@
   ./services/monitoring/hdaps.nix
   ./services/monitoring/heapster.nix
   ./services/monitoring/incron.nix
+  ./services/monitoring/jaeger.nix
   ./services/monitoring/kapacitor.nix
   ./services/monitoring/karma.nix
   ./services/monitoring/kener.nix
