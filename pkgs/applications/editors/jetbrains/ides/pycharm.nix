a9e867002d64f9c0876b8e5862e7b112e5c1fd38
Olli Helenius <liff@iki.fi>
2026-09-07 12:31:41 +0300

    Revert "jetbrains.pycharm{-oss}: migrate common overrides to hook"
    This reverts commit 6bf57108320b814e92d52ff7b19369ae50b86b64.

--- pkgs/applications/editors/jetbrains/ides/pycharm.nix
+++ pkgs/applications/editors/jetbrains/ides/pycharm.nix
@@ -5,8 +5,8 @@
   jetbrains,
   jetbrains-libdbm,
   fsnotifier,
+  pyCharmCommonOverrides,
   musl,
-  python3,
 }:
 let
   system = stdenv.hostPlatform.system;
@@ -27,7 +27,7 @@ let
   };
   # update-script-end: urls
 in
-jetbrains.mkJetBrainsProduct {
+(jetbrains.mkJetBrainsProduct {
   inherit jetbrains-libdbm fsnotifier;
 
   pname = "pycharm";
@@ -48,12 +48,6 @@ jetbrains.mkJetBrainsProduct {
       jetbrains.jdk-no-jcef
     else
       null;
-
-  nativeBuildInputs = [
-    python3
-    python3.pkgs.setuptools
-    jetbrains.cythonDebugSpeedupsHook
-  ];
 
   buildInputs = lib.optionals stdenv.hostPlatform.isLinux [
     musl
@@ -78,4 +72,5 @@ jetbrains.mkJetBrainsProduct {
       else
         [ lib.sourceTypes.binaryBytecode ];
   };
-}
+}).overrideAttrs
+  pyCharmCommonOverrides
